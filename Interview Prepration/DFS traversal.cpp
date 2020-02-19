418d43e42433a6d6cb0dac8292d8f9b9f790c395
Sourabh Majumdar <majumdars844@gmail.com>
2020-02-20 00:54:05 +0530

    BFS

--- Interview Prepration/DFS traversal.cpp
+++ Interview Prepration/DFS traversal.cpp
@@ -5,23 +5,15 @@ using namespace std;
 #define endl "\n" 
 typedef long long int ll;
 
-void BFS(vector<ll> graph[], bool * visited, ll sV)
+void DFS(vector<ll> graph[], bool * visited, ll sV)
 {
     visited[sV] = true;
-    queue<ll> que;
-    que.push(sV);
-    while(!que.empty())
+    cout << sV << " ";
+    for(ll i = 0; i < graph[sV].size(); i++)
     {
-        ll x = que.front();
-        cout << x << " ";
-        que.pop();
-        for(ll i = 0; i < graph[x].size(); i++)
+        if(!visited[graph[sV][i]])
         {
-            if(!visited[graph[x][i]])
-            {
-                que.push(graph[x][i]);
-                visited[graph[x][i]] = true;
-            }
+            DFS(graph, visited, graph[sV][i]);
         }
     }
 }
@@ -31,9 +23,9 @@ int main()
     fast;
     ll n;
     cin >> n;
-    vector<ll> graph[n + 1];
     ll e;
     cin >> e;
+    vector<ll> graph[n + 1];
     for(ll i = 0; i < e; i++)
     {
         ll x, y;
@@ -49,9 +41,10 @@ int main()
     {
         if(!visited[i])
         {
-            BFS(graph, visited, i);
+            DFS(graph, visited, i);
         }
     }
+
 
     return 0;
 }
